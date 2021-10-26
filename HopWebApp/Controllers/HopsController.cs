1800d45bcb78ae3e3a498bcd249bad53c79187bb
Aleksander Kruse Schaumburg-Müller <alek1082@edu.easj.dk>
2021-10-26 23:06:19 +0200

    Add project files.

---
+++ HopWebApp/Controllers/HopsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HopClassLib;
+using HopWebApp.Managers;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.HttpOverrides;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HopWebApp.Controllers
+{
+    //[EnableCors()]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class HopsController : ControllerBase
+    {
+
+        private readonly HopsManager _manager = new HopsManager();
+
+
+        // GET: api/<HopsController>
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public IEnumerable<Hop> GetAll()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<HopsController>/5
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]
+        public Hop GetById(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<HopsController>
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public Hop Post([Microsoft.AspNetCore.Mvc.FromBody] Hop value)
+        {
+            return _manager.Add(value);
+        }
+
+        // DELETE api/<HopsController>/5
+        [Microsoft.AspNetCore.Mvc.HttpDelete("{id}")]
+        public Hop Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ HopWebApp/Managers/HopsManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HopClassLib;
+
+namespace HopWebApp.Managers
+{
+    public class HopsManager
+    {
+
+        private static int _nextId = 1;
+
+        private static readonly List<Hop> Data = new List<Hop>
+        {
+            new Hop {Id = _nextId++, Name = "Amarillo", AlphaAcid = 7.5, HarvestYear = 2019, Price = 49},
+            new Hop {Id = _nextId++, Name = "Cascade US", AlphaAcid = 6.5, HarvestYear = 2020, Price = 69},
+            new Hop {Id = _nextId++, Name = "Chinook", AlphaAcid = 11.3, HarvestYear = 2020, Price = 65},
+        };
+
+
+        public List<Hop> GetAll()
+        {
+            return new List<Hop>(Data);
+        }
+
+        public Hop GetById(int id)
+        {
+            return Data.Find(Hop => Hop.Id == id);
+        }
+
+        public Hop Add(Hop newHop)
+        {
+            newHop.Id = _nextId++;
+            Data.Add(newHop);
+            return newHop;
+        }
+
+        public Hop Delete(int id)
+        {
+            Hop hop = Data.Find(hop1 => hop1.Id == id);
+            if (hop == null) return null;
+            Data.Remove(hop);
+            return hop;
+        }
+
+
+
+
+
+
+    }
+}
